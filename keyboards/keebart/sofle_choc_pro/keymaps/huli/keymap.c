a094435fe0517ad65bbe60f0e941c75ff6fe86bf
Huli <hulijingdev@gmail.com>
2025-11-08 13:08:07 +0100

    customize layers

--- keyboards/keebart/sofle_choc_pro/keymaps/huli/keymap.c
+++ keyboards/keebart/sofle_choc_pro/keymaps/huli/keymap.c
@@ -12,12 +12,8 @@ enum layers {
     EXTRA
 };
 
-#define BASE_RGB 10, 10, 10
-#define NUM_RGB 0, 0, 100
+#define DEFAULT_RGB 10, 10, 10
 #define GAME_RGB 100, 0, 0
-
-#define FUNC_RGB 0, 100, 0
-#define EXTRA_RGB 0, 100, 100
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /*
@@ -37,74 +33,133 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 [BASE] = LAYOUT_split_4x6_5(
-  KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_BSPC,
-  KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_LBRC,
-  MO(BASE_ARROW), KC_A,   KC_S,    KC_D,    KC_F,    KC_G,               KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_QUOT,
-  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, TG(GAME),    TG(NUM), KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_NUBS,
+  KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                       KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_BSPC,
+  KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                       KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_LBRC,
+  MO(BASE_ARROW), KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                 KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_QUOT,
+  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, TG(GAME),         TG(NUM), KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_NUBS,
                       KC_LGUI,KC_LALT,KC_LCTL, MO(FUNC), KC_ENT,      KC_SPC,  MO(EXTRA), KC_RCTL, KC_RALT, KC_RGUI
 ),
 [BASE_ARROW] = LAYOUT_split_4x6_5(
   _______,  _______,  _______,   _______,  _______,  _______,              _______,  _______,  _______,  _______,  _______,  _______,
   _______,  _______,   KC_UP,    _______,  _______,  _______,              _______,  _______,  _______,  _______,  _______,  _______,
   _______,   KC_LEFT,  KC_DOWN,  KC_RGHT,  _______,  _______,              _______,  _______,  _______,  _______,  _______,  _______,
-  _______, _______, _______, _______, _______, _______, TG(GAME),      TG(NUM), _______, _______, _______, _______, _______, _______,
-                     _______, _______, _______, MO(FUNC), _______,      _______,  MO(EXTRA), _______, _______, _______
+  _______, _______, _______, _______, _______, _______, _______,      _______, _______, _______, _______, _______, _______, _______,
+                     _______, _______, _______, _______, _______,      _______,  _______, _______, _______, _______
 ),
 
 [NUM] = LAYOUT_split_4x6_5(
-  _______, _______ , _______ , _______ , _______ , _______,                       _______,  KC_NUM, KC_PSLS,  KC_PAST,  KC_PMNS, _______,
-  _______,  KC_INS,  KC_PSCR,   KC_APP,  XXXXXXX, XXXXXXX,                        KC_PGUP, KC_P7, KC_P8, KC_P9, KC_PPLS, KC_BSPC,
-  _______, KC_LALT,  KC_LCTL,  KC_LSFT,  XXXXXXX, KC_CAPS,                        KC_PGDN,  KC_P4, KC_P5, KC_P6,  KC_PDOT, KC_BSPC,
-  _______, C(KC_Z), C(KC_X), C(KC_C), C(KC_V), XXXXXXX,  _______,       _______,  XXXXXXX, KC_P1, KC_P2, KC_P3,  KC_P0, KC_PENT,
+  _______, _______ , _______ , _______ , _______ , _______,                 _______,  KC_NUM, KC_PSLS,  KC_PAST,  KC_PMNS, _______,
+  _______,  KC_INS,  KC_PSCR,   KC_APP,  XXXXXXX, XXXXXXX,                  KC_PGUP, KC_P7, KC_P8, KC_P9, KC_PPLS, KC_BSPC,
+  _______, KC_LALT,  KC_LCTL,  KC_LSFT,  XXXXXXX, KC_CAPS,                  KC_PGDN,  KC_P4, KC_P5, KC_P6,  KC_PDOT, KC_BSPC,
+  _______, C(KC_Z), C(KC_X), C(KC_C), C(KC_V), XXXXXXX,  _______,      _______,  XXXXXXX, KC_P1, KC_P2, KC_P3,  KC_P0, KC_PENT,
                     KC_LGUI,KC_LALT,KC_LCTL, MO(FUNC), KC_ENT,      KC_SPC,  MO(EXTRA), KC_RCTL, KC_RALT, KC_RGUI
 ),
 [GAME] = LAYOUT_split_4x6_5(
-  KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_BSPC,
-  KC_T, KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,                         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_LBRC,
-  KC_G, KC_LCTL,  KC_A,   KC_S,    KC_D,    KC_F,                        KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_QUOT,
-  KC_B, KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,     TG(GAME),    TG(NUM), KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_NUBS,
+  KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_BSPC,
+  KC_T, KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,                           KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_LBRC,
+  KC_G, KC_LCTL,  KC_A,   KC_S,    KC_D,    KC_F,                           KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_QUOT,
+  KC_B, KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,     _______,        _______, KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_NUBS,
                     KC_LGUI,KC_LALT,KC_GRV, MO(FUNC), KC_SPC,      KC_ENT,  MO(EXTRA), KC_RCTL, KC_RALT, KC_RGUI
 ),
 [FUNC] = LAYOUT_split_4x6_5(
-  _______,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                       KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,
-  KC_GRV,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_F12,
-  _______, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,                       KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PIPE,
-  _______,  KC_EQL, KC_MINS, KC_PLUS, KC_LCBR, KC_RCBR, _______,       _______, KC_LBRC, KC_RBRC, KC_SCLN, KC_COLN, KC_BSLS, _______,
+  _______,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                      KC_F1,   KC_F2,   KC_F3,   KC_F4,  KC_F5,  KC_F6,
+  KC_GRV,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,                 KC_F7,    KC_F8,    KC_F9,    KC_F10,    KC_F11,  KC_F12,
+  _______, KC_EXLM,  KC_QUESTION, KC_TILDE,  KC_AT, KC_PERC,                 RM_TOGG, RM_PREV, RM_NEXT, KC_LPRN, KC_RPRN, KC_PIPE,
+  _______,  KC_EQL, KC_MINS, KC_PLUS, KC_LCBR, KC_RCBR, _______,       _______, RM_VALD, RM_VALU, RALT(KC_MINS), KC_RBRC, KC_BSLS, _______,
                        _______, _______, _______, _______, _______,       _______, _______, _______, _______, _______
 ),
 [EXTRA] = LAYOUT_split_4x6_5(
-  _______, _______ , _______ , _______ , _______ , _______,                       _______,  _______, _______,  _______,  _______, _______,
-  _______,  TO(BASE),  TO(NUM),  XXXXXXX,  XXXXXXX, XXXXXXX,                        KC_PGUP, XXXXXXX,   KC_UP, XXXXXXX,C(KC_BSPC), KC_BSPC,
-  _______, KC_LALT,  KC_LCTL,  KC_LSFT,  XXXXXXX, KC_CAPS,                        KC_PGDN,  KC_LEFT, KC_DOWN, KC_RGHT,  KC_DEL, KC_BSPC,
+  _______, _______ , _______ , _______ , _______ , _______,                  _______,  _______, _______,  _______,  _______, _______,
+  _______,  TO(BASE),  TO(NUM),  XXXXXXX,  XXXXXXX, XXXXXXX,                 KC_PGUP, XXXXXXX,   KC_UP, XXXXXXX,C(KC_BSPC), KC_BSPC,
+  _______, KC_LALT,  KC_LCTL,  KC_LSFT,  XXXXXXX, KC_CAPS,                   KC_PGDN,  KC_LEFT, KC_DOWN, KC_RGHT,  KC_DEL, KC_BSPC,
   _______, C(KC_Z), C(KC_X), C(KC_C), C(KC_V), XXXXXXX,  _______,       _______,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,   XXXXXXX, _______,
                          _______, _______, _______, _______, _______,       _______, _______, _______, _______, _______
 ),
 };
 
+
+/*
+ * RGB Mapping (other side mirrored)
+ * ,-----------------------------------------.
+ * |  26  |  25  |  17  |  16  |   7  |   6  |
+ * |------+------+------+------+------+------|
+ * |  27  |  24  |  18  |  15  |   8  |   5  |
+ * |------+------+------+------+------+------|
+ * |  28  |  23  |  19  |  14  |   9  |   4  |-------.
+ * |------+------+------+------+------+------|  0    |
+ * |  29  |  22  |  20  |  13  |  l0  |   3  |-------|
+ * `-----------------------------------------/       /
+ *            |  21  |  12  |  11  |  2   | /   1   /
+ *            |      |      |      |      |/       /
+ *            `----------------------------------'
+ */
+
+uint8_t wasd[] = {18, 23, 19, 14};
+uint8_t numNum[] = {22, 20, 13, 10, 19, 14, 9, 18, 15, 8};
+uint8_t numOp[] = {7, 16, 17, 25, 24};
+uint8_t funcKeysMaster[] = {25, 17, 16, 7, 6, 24, 18, 15, 8, 5};
+uint8_t funcKeySlave[] = {6, 7, 16, 17, 25, 26, 27, 24, 18, 15, 8, 5};
+uint8_t slaveArrow[] = {9, 14, 15, 19};
+
+void set_key_color(bool isMaster, uint8_t leds[], uint8_t length, uint8_t red, uint8_t green, uint8_t blue) {
+    if (is_keyboard_master() == !isMaster) {
+        return;
+    }
+    for (uint8_t i = 0; i < (int)length; i++) {
+        rgb_matrix_set_color(leds[i], red, green, blue);
+    }
+};
+
+void set_base_arrow_key_color(void) {
+    rgb_matrix_set_color_all(DEFAULT_RGB);
+    set_key_color(true, wasd, 4, 100, 0, 0);
+};
+
+void set_game_key_color(void) {
+    rgb_matrix_set_color_all(GAME_RGB);
+    set_key_color(true, wasd, 4, 100, 100, 100);
+};
+
+void set_num_key_color(void) {
+    rgb_matrix_set_color_all(DEFAULT_RGB);
+    set_key_color(false, numNum, 10, 0, 0, 100);
+    set_key_color(false, numOp, 5, 0, 100, 100);
+    set_key_color(false, (uint8_t[]) { 23 }, 1, 0, 100, 50);
+    set_key_color(false, (uint8_t[]) { 27,28 }, 2, 100, 0, 0);
+    set_key_color(false, (uint8_t[]) { 29 }, 1, 0, 100, 0);
+};
+
+void set_func_key_color(void) {
+    rgb_matrix_set_color_all(DEFAULT_RGB);
+    set_key_color(true, funcKeysMaster, 10, 100, 0, 0);
+    set_key_color(false, funcKeySlave, 12, 100, 0, 0);
+};
+
+void set_extra_key_color(void) {
+    rgb_matrix_set_color_all(DEFAULT_RGB);
+    set_key_color(false, slaveArrow, 4, 0, 100, 0);
+};
+
 bool rgb_matrix_indicators_user(void) {
-  uint8_t layer = biton32(layer_state);
+  uint8_t layer = get_highest_layer(layer_state);
   switch (layer) {
     case NUM:
-      rgb_matrix_set_color_all(NUM_RGB);
+      set_num_key_color();
       break;
     case GAME:
-      rgb_matrix_set_color_all(GAME_RGB);
+      set_game_key_color();
       break;
     case FUNC:
-      rgb_matrix_set_color_all(FUNC_RGB);
+      set_func_key_color();
       break;
     case EXTRA:
-      rgb_matrix_set_color_all(EXTRA_RGB);
+      set_extra_key_color();
       break;
     case BASE_ARROW:
-      rgb_matrix_set_color_all(BASE_RGB);
-    //   uint8_t wasd[] = {14, 18, 19, 23};
-    //   for (uint8_t i = 0; i < 4; i++){
-    //     rgb_matrix_set_color(wasd[i],255,0,0);
-    //   }
+      set_base_arrow_key_color();
       break;
     default:
-      rgb_matrix_set_color_all(BASE_RGB);
+      rgb_matrix_set_color_all(DEFAULT_RGB);
       break;
   }
   return true;
