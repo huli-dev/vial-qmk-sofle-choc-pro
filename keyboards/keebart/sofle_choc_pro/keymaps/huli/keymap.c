0cf63b3c4faed772d14cef7b49a3043798225a74
Huli <hulijingdev@gmail.com>
2025-11-08 13:21:42 +0100

    fixed rgb for gaming layer

--- keyboards/keebart/sofle_choc_pro/keymaps/huli/keymap.c
+++ keyboards/keebart/sofle_choc_pro/keymaps/huli/keymap.c
@@ -37,7 +37,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                       KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_LBRC,
   MO(BASE_ARROW), KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                 KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_QUOT,
   KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, TG(GAME),         TG(NUM), KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_NUBS,
-                      KC_LGUI,KC_LALT,KC_LCTL, MO(FUNC), KC_ENT,      KC_SPC,  MO(EXTRA), KC_RCTL, KC_RALT, KC_RGUI
+                      KC_LGUI, KC_LALT,KC_LCTL, MO(FUNC), KC_ENT,      KC_SPC,  MO(EXTRA), KC_RCTL, KC_RALT, KC_RGUI
 ),
 [BASE_ARROW] = LAYOUT_split_4x6_5(
   _______,  _______,  _______,   _______,  _______,  _______,              _______,  _______,  _______,  _______,  _______,  _______,
@@ -52,14 +52,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______,  KC_INS,  KC_PSCR,   KC_APP,  XXXXXXX, XXXXXXX,                  KC_PGUP, KC_P7, KC_P8, KC_P9, KC_PPLS, KC_BSPC,
   _______, KC_LALT,  KC_LCTL,  KC_LSFT,  XXXXXXX, KC_CAPS,                  KC_PGDN,  KC_P4, KC_P5, KC_P6,  KC_PDOT, KC_BSPC,
   _______, C(KC_Z), C(KC_X), C(KC_C), C(KC_V), XXXXXXX,  _______,      _______,  XXXXXXX, KC_P1, KC_P2, KC_P3,  KC_P0, KC_PENT,
-                    KC_LGUI,KC_LALT,KC_LCTL, MO(FUNC), KC_ENT,      KC_SPC,  MO(EXTRA), KC_RCTL, KC_RALT, KC_RGUI
+                    KC_LGUI, KC_LALT,KC_LCTL, MO(FUNC), KC_ENT,      KC_SPC,  MO(EXTRA), KC_RCTL, KC_RALT, KC_RGUI
 ),
 [GAME] = LAYOUT_split_4x6_5(
   KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_BSPC,
   KC_T, KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,                           KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_LBRC,
   KC_G, KC_LCTL,  KC_A,   KC_S,    KC_D,    KC_F,                           KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_QUOT,
   KC_B, KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,     _______,        _______, KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_NUBS,
-                    KC_LGUI,KC_LALT,KC_GRV, MO(FUNC), KC_SPC,      KC_ENT,  MO(EXTRA), KC_RCTL, KC_RALT, KC_RGUI
+                    KC_LGUI, KC_GRV, KC_LALT, MO(FUNC), KC_SPC,      KC_ENT,  MO(EXTRA), KC_RCTL, KC_RALT, KC_RGUI
 ),
 [FUNC] = LAYOUT_split_4x6_5(
   _______,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                      KC_F1,   KC_F2,   KC_F3,   KC_F4,  KC_F5,  KC_F6,
@@ -99,7 +99,7 @@ uint8_t numNum[] = {22, 20, 13, 10, 19, 14, 9, 18, 15, 8};
 uint8_t numOp[] = {7, 16, 17, 25, 24};
 uint8_t funcKeysMaster[] = {25, 17, 16, 7, 6, 24, 18, 15, 8, 5};
 uint8_t funcKeySlave[] = {6, 7, 16, 17, 25, 26, 27, 24, 18, 15, 8, 5};
-uint8_t slaveArrow[] = {9, 14, 15, 19};
+uint8_t centeredArrow[] = {9, 14, 15, 19};
 
 void set_key_color(bool isMaster, uint8_t leds[], uint8_t length, uint8_t red, uint8_t green, uint8_t blue) {
     if (is_keyboard_master() == !isMaster) {
@@ -117,7 +117,7 @@ void set_base_arrow_key_color(void) {
 
 void set_game_key_color(void) {
     rgb_matrix_set_color_all(GAME_RGB);
-    set_key_color(true, wasd, 4, 100, 100, 100);
+    set_key_color(true, centeredArrow, 4, 100, 100, 100);
 };
 
 void set_num_key_color(void) {
@@ -137,7 +137,7 @@ void set_func_key_color(void) {
 
 void set_extra_key_color(void) {
     rgb_matrix_set_color_all(DEFAULT_RGB);
-    set_key_color(false, slaveArrow, 4, 0, 100, 0);
+    set_key_color(false, centeredArrow, 4, 0, 100, 0);
 };
 
 bool rgb_matrix_indicators_user(void) {
