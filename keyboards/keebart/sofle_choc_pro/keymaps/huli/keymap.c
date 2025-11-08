6170e5a51deec8b742695fbea4a13ae39177e363
Huli <hulijingdev@gmail.com>
2025-11-08 22:53:33 +0100

    adjusted gaming layer

--- keyboards/keebart/sofle_choc_pro/keymaps/huli/keymap.c
+++ keyboards/keebart/sofle_choc_pro/keymaps/huli/keymap.c
@@ -64,9 +64,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [GAME] = LAYOUT_split_4x6_5(
   KC_GRV,   KC_1,     KC_2,   KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_BSPC,
   KC_TAB,   KC_T,     KC_Q,   KC_W,    KC_E,    KC_R,                         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_LBRC,
-  KC_G,     KC_B,     KC_A,   KC_S,    KC_D,    KC_F,                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_QUOT,
-  KC_M,     KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,   _______,     _______, KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_RBRC,
-                    KC_LCTL, KC_ESC, KC_LALT, MO(FUNC), KC_SPC,      KC_ENT,  MO(EXTRA), KC_RCTL, KC_RALT, KC_RGUI
+  KC_G,     KC_LSFT,     KC_A,   KC_S,    KC_D,    KC_F,                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_QUOT,
+  KC_B,     KC_LCTL,  KC_Z,   KC_X,    KC_C,    KC_V,   _______,     _______, KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_RBRC,
+                KC_PSCR, KC_ESC, KC_LALT, MO(FUNC), KC_SPC,      KC_ENT,  MO(EXTRA), KC_RCTL, KC_RALT, KC_RGUI
 ),
 [FUNC] = LAYOUT_split_4x6_5(
   TO(EXTRA),   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                      KC_F1,   KC_F2,   KC_F3,   KC_F4,  KC_F5,  KC_F6,
@@ -77,8 +77,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 [EXTRA] = LAYOUT_split_4x6_5(
   A(KC_F4), _______, _______, _______, _______, LCA(KC_DEL),                RM_TOGG,  _______, _______,  _______,  _______, KC_PSCR,
-  _______, TO(BASE), TO(SPCENT), TO(FUNC), XXXXXXX, XXXXXXX,                KC_PGUP,  KC_INS,  KC_UP,   KC_DEL, C(KC_BSPC), KC_BSPC,
-  _______, C(KC_Y), C(KC_Z),  XXXXXXX,  XXXXXXX, KC_CAPS,                   KC_PGDN,  KC_LEFT, KC_DOWN, KC_RGHT,  KC_DEL, KC_BSPC,
+  _______, TO(BASE), TO(SPCENT), TO(FUNC), XXXXXXX, XXXXXXX,                KC_PGUP,  KC_INS,  KC_UP,   KC_DEL, C(KC_BSPC), C(KC_BSPC),
+  _______, C(KC_Y), C(KC_Z),  XXXXXXX,  XXXXXXX, KC_CAPS,                   KC_PGDN,  KC_LEFT, KC_DOWN, KC_RGHT,  C(KC_BSPC), C(KC_BSPC),
   _______, C(KC_X), C(KC_C), C(KC_V), XXXXXXX, XXXXXXX,  _______,      _______,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,   XXXXXXX, KC_PWR,
                     _______, _______, _______, _______, _______,       _______, _______, _______, _______, _______
 ),
